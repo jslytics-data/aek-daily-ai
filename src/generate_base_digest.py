8392bcff2f0b5df4c4f393094f78df1e899e0dc2
jslytics <jason@jslytics.com>
2025-06-27 15:13:13 +0000

    summary top

--- src/generate_base_digest.py
+++ src/generate_base_digest.py
@@ -29,10 +29,11 @@ NEWSLETTER_PROMPT_GENERATION_INSTRUCTION_TEMPLATE = """
         - Output content in source language of the provided article titles.
         - Exclude articles not related to {{{{topic}}}}. Careful to only focus on the topic.
         - Reference links inline wherever useful to users. Good to give credit to the journalists and platforms, but not too intrusive for our users. Prioritise linking topical and high reputable relevant sources. Try not to show sources/links from opposing sources. For example if the newsletter is about Manchester United, don’t show links from an Arsenal or London domain, unless there's clear value(e.g. tickets, fixture etc). Generally give priority to latest articles.
-        - Output in nicely formatted modern email HTML, like Modern Brew. Make sure its mobile friendly. For mobile views, minimise side margin so to maximise text space.
+        - Output in nicely formatted modern email HTML, like Modern Brew. Make sure its mobile friendly. For mobile views, IMPORTANT: MINIMISE side margin so to maximise text space.
         - Follow the topic’s colors, but don't overdo it.
         - Try to mimic the tone suggested by the article titles. Don’t be cringy. Don’t overdo with greeting, you can get straight to it. Make the tone natural as if a human topic journalist with deep knowledge and native style and tone would edit it. If you really think it helps, use Emoji's but don't overdo it. Keep in mind that a portion of the audience would have already read the headlines online, so maybe include interesting details if you find them, so there's value for them as well.
         - Enhance Readability: Ensure text is clear and concise. Vary sentence structure for natural flow, and use subheadings to break up content, making it scannable and easy to digest. Think nuance, think of neuroscience and serotonin and dopamine. Use formatting extensively to improve readability.
+        - If you think it would be nice, you can have a "quick summary" type of section at the beginning
         - Make the UI like a world class UI designer would design it, but keep it simple. Add in some magic!
         - Do not include images
 
